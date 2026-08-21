fa4b90a5d70d39803fd68e993aa7936da8cd5b95
juncpa <iwbasm92@gmail.com>
2026-08-21 09:08:28 +0900

    chore(intelligence): export live collection runner

--- src/intelligence_core/index.ts
+++ src/intelligence_core/index.ts
@@ -3,4 +3,5 @@ export * from "./store.js";
 export * from "./feature_registry.js";
 export * from "./eth_value_capture_adapter.js";
 export * from "./eth_value_capture_collector.js";
+export * from "./collection_run.js";
 export * from "./history.js";
