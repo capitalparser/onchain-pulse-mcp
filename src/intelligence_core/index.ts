2cb15b6666d0072805bfd8d036dae2448ce49744
juncpa <iwbasm92@gmail.com>
2026-08-21 09:05:18 +0900

    chore(intelligence): export ETH forward collector

--- src/intelligence_core/index.ts
+++ src/intelligence_core/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./store.js";
 export * from "./feature_registry.js";
 export * from "./eth_value_capture_adapter.js";
+export * from "./eth_value_capture_collector.js";
 export * from "./history.js";
