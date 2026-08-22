83c3fe2ae9ee27b681be35437dff2f402513ccd5
juncpa <iwbasm92@gmail.com>
2026-08-23 04:38:47 +0900

    feat(intelligence): export Ethereum ecosystem capture adapter

--- src/intelligence_core/index.ts
+++ src/intelligence_core/index.ts
@@ -2,6 +2,7 @@ export * from "./types.js";
 export * from "./store.js";
 export * from "./feature_registry.js";
 export * from "./eth_value_capture_adapter.js";
+export * from "./eth_ecosystem_capture_adapter.js";
 export * from "./eth_value_capture_collector.js";
 export * from "./collection_run.js";
 export * from "./history.js";
